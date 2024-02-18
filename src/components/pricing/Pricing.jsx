fb7d476e031b2a18281bc8f8cbe75f7b18127c4b
Sehs <Hassen.b.a97@gmail.com>
2024-02-18 21:20:02 +0100

    change

--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -1,21 +1,36 @@
-import React from "react"
-import Back from "../common/back/Back"
-import PriceCard from "./PriceCard"
-import "./price.css"
-import Faq from "./Faq"
+import React from "react";
+import Back from "../common/back/Back";
+
+import "./price.css";
+
+import universitiesData from "./uni.json"; // Import the university data
 
 const Pricing = () => {
   return (
     <>
       <Back title='Choose The Right Plan' />
-      <section className='price padding'>
-        <div className='container grid'>
-          <PriceCard />
-        </div>
+      <section className='price paddng'>
+        <h2>Universities and Degrees</h2>
+        {Object.values(universitiesData).map((university, index) => (
+          <div key={index}>
+            <h3>{university.university_name}</h3>
+            <ul>
+              {Object.values(university.Faculties).map((faculty, index) => (
+                <li key={index}>
+                  <h4>{faculty.faculty_name}</h4>
+                  <ul>
+                    {faculty.degrees.map((degree, index) => (
+                      <li key={index}>{degree}</li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </section>
-      <Faq />
     </>
-  )
-}
+  );
+};
 
-export default Pricing
+export default Pricing;
